e091927eb4b33cfde3c8d4dd12a3e8fa1cda46d2
simranmalik-4 <simranmalik@google.com>
2020-07-08 10:33:08 +0000

    removed accidental commenting in dataset_generator

--- benchmarking/dataset_generator.py
+++ benchmarking/dataset_generator.py
@@ -8,9 +8,9 @@ class DatasetGenerator:
         self.root_node = InternalNode(depth_in_tree = 0, parameter_to_value = {})
         data = self.root_node.create_child_nodes()
 
-        # with open(Parameter.DATASET_FILE_NAME, 'w') as csv_file:  
-        #     csv_writer = csv.writer(csv_file)
-        #     csv_writer.writerow(["Metadata", "Data"])
-        #     csv_writer.writerows(data)
+        with open(Parameter.DATASET_FILE_NAME, 'w') as csv_file:  
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["Metadata", "Data"])
+            csv_writer.writerows(data)
 
 datasetGenerator = DatasetGenerator()
